e35133e38ba1649f1b37fc8a2281ccfa296e494d
ksuka <ksikacheva@yandex.ru>
2025-02-22 10:54:02 +0300

    billiard

--- js/js.js
+++ js/js.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
-let roulette = document.querySelector(".roulette");
+/*let roulette = document.querySelector(".roulette");
 let btn = document.getElementById("btn");
 let number = Math.ceil(Math.random() * 1000);
 
@@ -11,7 +11,35 @@ btn.onclick = function () {
     roulette.style.transform = "rotate(" + number + "deg)";
   number += Math.ceil(Math.random() * 1000);
 
-};
+};*/
+
+/*const one = this.getElementById("one");
+let rotation = 0;
+
+one.addEventListener('click', function(){
+  rotation += 45;
+  one.style.transform = 'rotate(${rotation}deg)';
+
+});
+*/
+
+/*const one = document.getElementById("one");
+
+one.addEventListener("click", function moveElement() {
+  one.style.left = '5vw';
+
+})*/
+
+
+
+/*const one = document.getElementById("one");
+  let rotation = 0;
+
+  one.addEventListener("click", function() {
+    rotation += 90; // Rotate by 90 degrees each click
+    one.style.transform = `rotate(${rotation}deg)`;
+  });*/
+
 
 });
 
