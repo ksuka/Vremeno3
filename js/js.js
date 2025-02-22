5097e45e5d496d8ef02917c78a4ed6869750077a
ksuka <ksikacheva@yandex.ru>
2025-02-22 23:15:06 +0300

    section2

--- js/js.js
+++ js/js.js
@@ -32,14 +32,54 @@ one.addEventListener("click", function moveElement() {
 
 
 
-/*const one = document.getElementById("one");
+const dicebubi = document.getElementById("dicebubi");
   let rotation = 0;
 
-  one.addEventListener("click", function() {
-    rotation += 90; // Rotate by 90 degrees each click
-    one.style.transform = `rotate(${rotation}deg)`;
-  });*/
+  dicebubi.addEventListener("click", function() {
+    rotation += 45; // Rotate by 90 degrees each click
+   dicebubi.style.transform = `rotate(${rotation}deg)`;
+  });
 
+
+
+  const dicetrefi = document.getElementById("dicetrefi");
+  let rotate = 0;
+
+  dicetrefi.addEventListener("click", function() {
+    rotate += -45; // Rotate by 90 degrees each click
+    dicetrefi.style.transform = `rotate(${rotate}deg)`;
+  });
+
+
+
+  const dicechervi = document.getElementById("dicechervi");
+  let rotat = 0;
+
+  dicechervi.addEventListener("click", function() {
+    rotat += -45; 
+    dicechervi.style.transform = `rotate(${rotat}deg)`;
+  });
+
+
+
+ const dicepiki = document.getElementById("dicepiki");
+  let rota = 0;
+
+  dicepiki.addEventListener("click", function() {
+    rota += 45; 
+    dicepiki.style.transform = `rotate(${rota}deg)`;
+  });
+
+
+  const dicejoker = document.getElementById("dicejoker");
+  let rot = 0;
+
+  dicejoker.addEventListener("click", function() {
+    rot += 45; 
+    dicejoker.style.transform = `rotate(${rot}deg)`;
+  });
+  
+  
 
 });
 
